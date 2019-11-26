dce8c3f26db300e3882405b8fd1f22a2c49f2abe
MadaraKalpina <53853465+MadaraKalpina@users.noreply.github.com>
2019-11-26 22:38:13 +0200

    improved code but still some bugs with the first two input fields

--- main.js
+++ main.js
@@ -1,40 +1,50 @@
 function main () {
-    const container = document.querySelector('#container');
-    var min = document.querySelector('#min');
-    var max = document.querySelector('#max');
-    var fizz = document.querySelector('#fizz');
-    var buzz = document.querySelector('#buzz');
-
-    console.log("this is befor the default elements are genetared");
     fizzBuzz();
-    console.log("default elemets are: min value:" + min.value + "; max value: " + max.value + "; fizz value: " + fizz.value + "; buzz value: " + buzz.value);
-
-    const generate = document.querySelector("#generate")
-    // have to add an if if the values are empty
-    console.log("we have made a button");
-    generate.addEventListener('click', event => {
-        console.log("we clicked on the generate button");
-        container.innerHTML = ''; 
-        console.log("just emptied the container div");
-        fizzBuzz();
-        console.log("default elemets are: min value:" + min.value + "; max value: " + max.value + "; fizz value: " + fizz.value + "; buzz value: " + buzz.value);
-      });
+    console.log("this is after the default elements are genetared");
+    console.log("min value: " + min.value);
+    console.log("max value: " + max.value);
+    console.log("fizz value: " + fizz.value);
+    console.log("buzz value: " + buzz.value);
     
-
-    const clear = document.querySelector("#clear")
-    console.log("we have made a clear all button");
-    clear.addEventListener('click', event => {
-        console.log("we clicked on the clear all button");
-        container.innerHTML = ''; 
-        console.log("just emptied the container div");
-        min.value = '1';    
-        max.value = '100'; 
-        fizz.value = '3'; 
-        buzz.value = '5';  
-        console.log("all values are empty");
-      });
+    addHandlers ();
+    console.log("Added event handlers");    
 }
 
+function addHandlers () {
+
+    document.querySelector("#generate").onclick = () => {
+        console.log("we clicked on the generate button");
+        clearDivs();
+        console.log("it cleared the previous divs");
+        fizzBuzz(); 
+        console.log("it generated these divs");
+    }
+
+    document.querySelector("#clear").onclick = () => {
+        console.log("we clicked on the clear all button");
+        clearDivs();
+        resetValues(); 
+    }
+}
+
+function clearDivs () {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    console.log("just emptied the container div");
+}
+
+function resetValues () {
+    min.value = 1;    
+    max.value = 100; 
+    fizz.value = 3; 
+    buzz.value = 5; 
+    console.log("just reset the values to default");
+    console.log("min value: " + min.value);
+    console.log("max value: " + max.value);
+    console.log("fizz value: " + fizz.value);
+    console.log("buzz value: " + buzz.value);
+}
 
 function addElemObj (parent, obj) {
     const elem = document.createElement(obj.tag); 
@@ -46,8 +56,13 @@ function addElemObj (parent, obj) {
 }
 
 function fizzBuzz () {
+    console.log("fizbuzz started");
+    const container = document.querySelector('#container');
+    const min = document.querySelector('#min');
+    const max = document.querySelector('#max');
+    const fizz = document.querySelector('#fizz');
+    const buzz = document.querySelector('#buzz');
 
-    // add new div elements inside container element
     for (let i = min.value; i <= max.value; i++) {
         const boxDiv = addElemObj(container, {id: "box" + i, tag: 'div', innerText: "box " + i, classList: "box"});
         if (i % fizz.value == 0 && i % buzz.value == 0) {
@@ -67,7 +82,11 @@ function fizzBuzz () {
             boxDiv.innerText = "Buzz";  
         }
     }
+    console.log("min value: " + min.value);
+    console.log("max value: " + max.value);
+    console.log("fizz value: " + fizz.value);
+    console.log("buzz value: " + buzz.value);
+    console.log("fizbuzz finished");
 }
-
 
 main ();
